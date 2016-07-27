885d637a37404cb4e98316f480e712e22bed8d16
Nikita <iam@niklucky.com>
2016-07-27 18:43:14 +0300

    0.5.5 Enabling Travis CI, Starting to implement tests

---
+++ tests/units/router.js
@@ -0,0 +1,11 @@
+'use strict';
+let assert = require('chai').assert;
+let mqb = require('../../index.js');
+var router = require('../../src/core/router');
+//var mocks = require('./mocks.js');
+
+describe('Router', function() {
+  it('Empty route', function() {
+
+  });
+});
